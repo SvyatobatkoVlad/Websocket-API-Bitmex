a55f3a4c23b87a7bb01a233d4c1fdc322f543f61
Vladyslav Sviatobatko <vladyslav.sviatobatko@technorely.com>
2021-12-04 17:51:16 +0200

    added gorilla/websocket

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/SvyatobatkoVlad/Websocket-API-Bitmex/pkg/logging"
 	"github.com/gin-gonic/gin"
+	Bitmex "github.com/SvyatobatkoVlad/Websocket-API-Bitmex/internal/bitmex"
 )
 
 func HomePage(c *gin.Context) {
@@ -15,7 +16,15 @@ func main() {
 	logger := logging.GetLogger()
 
 	r := gin.Default()
-	r.GET("/", HomePage)
+	r.LoadHTMLFiles("index.html")
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(200, "index.html", nil)
+	})
+
+	r.GET("/ws", func(c *gin.Context) {
+		Bitmex.Wshandler(c.Writer, c.Request)
+	})
 
 	logger.Info("run localhost:8080")
 	r.Run()
--- internal/bitmex/bitmex.go
+++ internal/bitmex/bitmex.go
@@ -1,5 +1,32 @@
 package bitmex
 
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"net/http"
+)
+
 type ResponceMessage struct {
 
+}
+
+var Wsupgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func Wshandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := Wsupgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("Failed to set websocket upgrade: %+v", err)
+		return
+	}
+
+	for {
+		t, msg, err := conn.ReadMessage()
+		if err != nil {
+			break
+		}
+		conn.WriteMessage(t, msg)
+	}
 }
